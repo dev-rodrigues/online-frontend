2e2749f92d0e42d302054de9ca44b47a83f29d5d
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2020-08-03 15:50:33 -0300

    confirmando email

--- src/pages/CadastroConfirmar/Index.tsx
+++ src/pages/CadastroConfirmar/Index.tsx
@@ -1,15 +1,73 @@
-import React from 'react';
+import React, {
+   useEffect,
+   useState,
+} from 'react';
 import { useRouteMatch } from 'react-router-dom';
+
+import { useToast } from '../../hooks/ToastContext';
+import api from '../../services/api';
+
+import Background from '../../components/atoms/Background/Index';
+import Root from '../../components/atoms/Root/Index';
+import Content from '../../components/atoms/Content/Index';
+import Title from '../../components/atoms/Title/SimpleTitle/Index';
+
 
 interface CadastroConfirmarParams {
   idConfirmacao: string,
 }
 
+interface teste {
+  'email-confirmacao': string,
+}
+
 const CadastroConfirmar: React.FC = () => {
 
   const { params } = useRouteMatch<CadastroConfirmarParams>();
+  const { addToast } = useToast();
+  const [emailConfirmacao, setEmailConfirmacao] = useState('');
 
-  return <h1>Confirmação de Email: {params.idConfirmacao}</h1>
+
+  useEffect( () => {
+
+    async function loadData() : Promise<void> {
+
+      try {
+        const response = await api.get<teste>(`cadastrar/confirmar/${params.idConfirmacao}`);
+        const email = response.data["email-confirmacao"]
+        setEmailConfirmacao(email);
+
+      } catch(err) {
+        addToast({
+          type: 'error',
+          title: 'Solicitação inválida',
+          description: 'Código de solicitação inválida'
+        })
+      }
+    }
+
+    loadData();
+
+  }, [params, addToast, setEmailConfirmacao]);
+
+
+
+  return (
+    <Background>
+      <Root>
+        <Content>
+        { emailConfirmacao !== '' ? (
+          <Title
+          titulo="Solicitação de Cadastro de Usuário"
+          subtitulo={`Seu email foi confirmado. Quando sua solicitação for processada um email de notificação será enviado para ${emailConfirmacao}`}/>
+        ) : (
+          <h1>deu ruim</h1>
+        )}
+
+        </Content>
+      </Root>
+    </Background>
+  );
 };
 
 export default CadastroConfirmar;
